ea8fec7a5a1704c25ed3a5443da1a243d84ab27b
Francisco Isola <franciscoisola1@gmail.com>
2022-04-28 16:24:21 -0300

    Todas las rutas creadas

--- routes/main.js
+++ routes/main.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const mainController = require ('../controllers/mainController')
 
 router.get('/home', mainController.index)
-router.get('/productDetail', mainController.productDetail)
-router.get('/productCart', mainController.productCart)
+router.get('/product-detail', mainController.productDetail)
+router.get('/rental-cart', mainController.productCart)
 router.get('/login', mainController.login)
 router.get('/register', mainController.register)
 
