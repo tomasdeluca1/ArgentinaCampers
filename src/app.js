8ea3d9a1edfd8355a1add20018539f67e8f41bc6
tomasdeluca1 <tomasdelucaa@gmail.com>
2022-06-13 22:33:10 -0300

    about us creado + funcion para agregar revieews al json desde el form

--- src/app.js
+++ src/app.js
@@ -47,6 +47,6 @@ app.use('/', aboutUs)
 
 
 //Inicializacion del servidor
-app.listen(3000, ()=>{
+app.listen(5000, ()=>{
     console.log("Server 5000 running");
 });
--- src/routes/aboutUs.js
+++ src/routes/aboutUs.js
@@ -1,12 +1,10 @@
 const express = require ('express');
 const router = express.Router();
 const aboutUsController= require ('../controllers/aboutUsController')
+const authMiddleware = require('../middlewares/authMiddleware')
 
 router.get('/aboutUs', aboutUsController.index)
-
-
-
-
+router.post('/aboutUs',authMiddleware, aboutUsController.create)
 
 
 module.exports = router; 
